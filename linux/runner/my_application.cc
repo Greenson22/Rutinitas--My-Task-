91f8ee5cb4a769b606f4f411127c268b8d715fd0
Frendy22 <greenson22022002@gmail.com>
2026-05-30 22:42:41 +0800

    feat: menghilangkan window title bar pada desktop Linux
    - Menambahkan konfigurasi `gtk_window_set_decorated(window, FALSE)` pada file `linux/my_application.cc` untuk menyembunyikan dekorasi jendela bawaan (title bar dan tombol kontrol).
    - Menyinkronkan penyesuaian properti title pada widget MaterialApp di dalam file `lib/main.dart` agar struktur internal aplikasi tetap konsisten.

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -26,6 +26,9 @@ static void my_application_activate(GApplication* application) {
   GtkWindow* window =
       GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
 
+  // Sembunyikan window title bar (borderless)
+  gtk_window_set_decorated(window, FALSE);
+
   // Use a header bar when running in GNOME as this is the common style used
   // by applications and is the setup most users will be using (e.g. Ubuntu
   // desktop).
@@ -46,11 +49,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "rutinitasku");
+    gtk_header_bar_set_title(header_bar, "MyTasks");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "rutinitasku");
+    gtk_window_set_title(window, "MyTasks");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
